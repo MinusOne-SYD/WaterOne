c1048e8da469c3698b3effe936689196d16beaa6
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-09-10 21:48:43 +1000

    value onchange

--- waterone-app/src/App.js
+++ waterone-app/src/App.js
@@ -1,6 +1,34 @@
 import './App.css';
+import * as React from 'react';
 
 function App() {
+
+  const stateOptions = [
+    {value: 'NSW', text: 'New South Wales'},
+    {value: 'TA', text: 'Tasmania'},
+    {value: 'VIC', text: 'Victoria'},
+    {value: 'WA', text: 'Western Australia'},
+    {value: 'SA', text: 'South Australia'},
+    {value: 'QLD', text: 'Queensland'},
+  ]
+
+  const typeOptions = [
+    {value: 'A', text: 'A'},
+    {value: 'B', text: 'B'},
+    {value: 'C', text: 'C'}
+  ]
+
+  const [state, setState] = React.useState(stateOptions[0].value);
+  const [type, setType] = React.useState(typeOptions[0].value);
+
+  const handleState = event => {
+    setState(event.target.value);
+  };
+
+  const handleType = event => {
+    setType(event.target.value);
+  };
+
   return (
     <div className="App">
       <div className="header">
@@ -15,21 +43,22 @@ function App() {
         <form>
           <div className="form-body">
             <label for="states">State</label>
-            <select id="states">
-              <option>New South Wales</option>
-              <option>Tasmania</option>
-              <option>Victoria</option>
-              <option>Western Australia</option>
-              <option>South Australia</option>
-              <option>Queensland</option>
+            <select id="states" value={state} onChange={handleState}>
+              {stateOptions.map(stateOptions => (
+                <option>
+                  {stateOptions.text}
+                </option>
+              ))}
             </select>
             </div>
             <div className="form-body">
-            <label>Type</label>
-            <select id="Type">
-              <option>A</option>
-              <option>B</option>
-              <option>C</option>
+            <label for="types">Type</label>
+            <select id="types" value={type} onChange={handleType}>
+              {typeOptions.map(typeOptions => (
+                <option>
+                  {typeOptions.text}
+                </option>
+              ))}
             </select>
           </div>
         </form>

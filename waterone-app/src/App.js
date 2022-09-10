e57202aa52ba314a83de264a29c479853fbad668
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-09-10 19:53:37 +1000

    waterone site init

---
+++ waterone-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <header className="title">
+          WaterOne
+        </header>
+        <div className="subtitle">
+          Ⓒ 2022 MinusOne
+        </div>
+      </div>
+      <div className="body">
+        ASDF
+      </div>
+    </div>
+  );
+}
+
+export default App;

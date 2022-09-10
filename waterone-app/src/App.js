a1fe658e69c95dba4831117b338f76754a35fed0
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-09-11 00:05:32 +1000

    stuff

--- waterone-app/src/App.js
+++ waterone-app/src/App.js
@@ -5,7 +5,9 @@ function App() {
 
   const stateOptions = [
     {value: 'NSW', text: 'New South Wales'},
-    {value: 'TA', text: 'Tasmania'},
+    {value: 'NT', text: 'Northern Territory'},
+    {value: 'ACT', text: 'Australian Capital Territory'},
+    {value: 'TAS', text: 'Tasmania'},
     {value: 'VIC', text: 'Victoria'},
     {value: 'WA', text: 'Western Australia'},
     {value: 'SA', text: 'South Australia'},
@@ -30,39 +32,51 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div className="header">
-        <header className="title">
-          WaterOne
-        </header>
-        <div className="subtitle">
-          Ⓒ 2022 MinusOne
-        </div>
-      </div>
-      <div className="body">
-        <form>
-          <div className="form-body">
-            <label for="states">State</label>
-            <select id="states" value={state} onChange={handleStateChange}>
-              {stateOptions.map(stateOptions => (
-                <option>
-                  {stateOptions.text}
-                </option>
-              ))}
-            </select>
-            </div>
-            <div className="form-body">
-            <label for="types">Type</label>
-            <select id="types" value={type} onChange={handleTypeChange}>
-              {typeOptions.map(typeOptions => (
-                <option>
-                  {typeOptions.text}
-                </option>
-              ))}
-            </select>
+    <div className="app">
+      <div className="app-bg" />
+      <div className="app-content">
+        <div className="header">
+          <header className="title">
+            WaterOne
+          </header>
+          <div className="subtitle">
+            Ⓒ 2022 MinusOne
           </div>
-        </form>
+        </div>
+        <div className="body">
+          <span>Search for water sources near your business.</span>
+          <form>
+            <div className="form-body">
+              <label for="states">State</label>
+              <select id="states" value={state} onChange={handleStateChange}>
+                {stateOptions.map(stateOptions => (
+                  <option>
+                    {stateOptions.text}
+                  </option>
+                ))}
+              </select>
+              </div>
+              <div className="form-body">
+                <label for="suburb">Suburb</label>
+                <input type="text" id="suburb"></input>
+              </div>
+              <div className="form-body">
+                <label for="postcode">Postcode</label>
+                <input type="text" id="postcode"></input>
+              </div>
+              {/* <div className="form-body">
+              <label for="types">Type</label>
+              <select id="types" value={type} onChange={handleTypeChange}>
+                {typeOptions.map(typeOptions => (
+                  <option>
+                    {typeOptions.text}
+                  </option>
+                ))}
+              </select>
+            </div> */}
+          </form>
         <button>LOCATE</button>
+        </div>
       </div>
     </div>
   );
